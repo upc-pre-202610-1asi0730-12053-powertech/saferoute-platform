243b29e7000d92d4dc1e2e07118f67e9b8abb964
Frase <ytcarniy@gmail.com>
2026-06-20 18:39:15 -0500

    feat(notification): add get notification by id query handling implementation.

--- saferoute-powertech-platform/Powertech.Platform/Notifications/Application/Internal/QueryServices/NotificationQueryService.cs
+++ saferoute-powertech-platform/Powertech.Platform/Notifications/Application/Internal/QueryServices/NotificationQueryService.cs
@@ -1,13 +1,14 @@
 using Powertech.Platform.Notifications.Application.QueryServices;
 using Powertech.Platform.Notifications.Domain.Model.Aggregates;
 using Powertech.Platform.Notifications.Domain.Model.Queries;
+using Powertech.Platform.Notifications.Domain.Repositories;
 
 namespace Powertech.Platform.Notifications.Application.Internal.QueryServices;
 
-public class NotificationQueryService : INotificationQueryService
+public class NotificationQueryService(INotificationRepository notificationRepository) : INotificationQueryService
 {
     public async Task<Notification?> Handle(GetNotificationByIdQuery query, CancellationToken cancellationToken = default)
     {
-        throw new NotImplementedException();
+        return await notificationRepository.FindByIdAsync(query.NotificationId);
     }
 }
