48c6466c044d751c97a88484875dee89a9a7e820
Nickolas Ramirez <developernick02@gmail.com>
2026-06-20 01:36:28 -0500

    feat: added update for Fleet bounded context

--- saferoute-powertech-platform/Powertech.Platform/Fleet/Domain/Model/Entities/Vehicle.cs
+++ saferoute-powertech-platform/Powertech.Platform/Fleet/Domain/Model/Entities/Vehicle.cs
@@ -1,7 +1,7 @@
 ﻿using Safer_Route_Platform.Fleet.Domain.Model.ValueObjects;
 using Safer_Route_Platform.Shared.Domain.Model.ValueObjects;
 
-namespace Safer_Route_Platform.Fleet.Domain.Model.Entities;
+namespace Powertech.Platform.Fleet.Domain.Model.Entities;
 
 /// <summary>
 ///     Entity representing the vehicle assigned to operate a route.
