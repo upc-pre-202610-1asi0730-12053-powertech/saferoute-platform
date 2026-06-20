c85c3c19bf7515e41e1a09896093807703ec1954
Nickolas Ramirez <developernick02@gmail.com>
2026-06-20 01:45:55 -0500

    feat: added GetRoutesByOrganizationIdQuery for Fleet bounded context

---
+++ saferoute-powertech-platform/Powertech.Platform/Fleet/Domain/Model/Queries/GetRoutesByOrganizationIdQuery.cs
@@ -0,0 +1,8 @@
+﻿namespace Powertech.Platform.Fleet.Domain.Model.Queries;
+
+
+/// <summary>Query to retrieve all routes that belong to a given organization.</summary>
+/// <param name="OrganizationId">The organization identifier to filter routes by.</param>
+
+
+public record GetRoutesByOrganizationIdQuery(Guid OrganizationId);
