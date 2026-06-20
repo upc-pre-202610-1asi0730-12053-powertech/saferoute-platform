9a68170a04e73b4d73cae48c67a9be273becf029
Nickolas Ramirez <developernick02@gmail.com>
2026-06-20 15:09:48 -0500

    feat: added CreateRouteCommandFromResourceAssembler for Fleet bounded context

---
+++ saferoute-powertech-platform/Powertech.Platform/Fleet/Interfaces/Rest/Transform/CreateRouteCommandFromResourceAssembler.cs
@@ -0,0 +1,17 @@
+﻿namespace Powertech.Platform.Fleet.Interfaces.Rest.Transform;
+
+/// <summary>
+///     Assembler that converts a <see cref="CreateRouteResource" /> into a <see cref="CreateRouteCommand" />.
+/// </summary>
+public static class CreateRouteCommandFromResourceAssembler
+{
+    /// <summary>Converts the input resource into a domain command, parsing the organization Guid.</summary>
+    /// <param name="resource">The create-route resource. Must not be null.</param>
+    /// <returns>The resulting <see cref="CreateRouteCommand" />.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="resource" /> is null.</exception>
+    public static CreateRouteCommand ToCommandFromResource(CreateRouteResource resource)
+    {
+        ArgumentNullException.ThrowIfNull(resource);
+        return new CreateRouteCommand(Guid.Parse(resource.OrganizationId), resource.Name);
+    }
+}
