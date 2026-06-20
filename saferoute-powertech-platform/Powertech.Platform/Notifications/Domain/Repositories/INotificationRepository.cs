f7c14a10dcf2c218944dcdf7b45347bc9af50fd4
Frase <ytcarniy@gmail.com>
2026-06-20 18:38:08 -0500

    feat(notification): add notification repository contract.

---
+++ saferoute-powertech-platform/Powertech.Platform/Notifications/Domain/Repositories/INotificationRepository.cs
@@ -0,0 +1,8 @@
+using Powertech.Platform.Notifications.Domain.Model.Aggregates;
+using Powertech.Platform.Shared.Domain.Repositories;
+
+namespace Powertech.Platform.Notifications.Domain.Repositories;
+
+public interface INotificationRepository : IBaseRepository<Notification>
+{
+}
