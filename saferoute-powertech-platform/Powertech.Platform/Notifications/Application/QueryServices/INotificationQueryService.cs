87afd9d52583c67a76d96d4c586bc98c2cec2aff
Frase <ytcarniy@gmail.com>
2026-06-20 18:39:14 -0500

    feat(notification): add get notification by id query service contract.

---
+++ saferoute-powertech-platform/Powertech.Platform/Notifications/Application/QueryServices/INotificationQueryService.cs
@@ -0,0 +1,9 @@
+using Powertech.Platform.Notifications.Domain.Model.Aggregates;
+using Powertech.Platform.Notifications.Domain.Model.Queries;
+
+namespace Powertech.Platform.Notifications.Application.QueryServices;
+
+public interface INotificationQueryService
+{
+    Task<Notification?> Handle(GetNotificationByIdQuery query, CancellationToken cancellationToken = default);
+}
