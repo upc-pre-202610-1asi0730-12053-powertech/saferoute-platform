cba02bdeb6cf7ed3fb26e45801bb368ae9bdfc8b
Nickolas Ramirez <developernick02@gmail.com>
2026-06-20 01:30:43 -0500

    feat: added RemoveStopCommand for Fleet bounded context

---
+++ saferoute-powertech-platform/Powertech.Platform/Fleet/Domain/Model/Commands/RemoveStopCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace Safer_Route_Platform.Fleet.Domain.Model.Commands;
+
+/// <summary>
+///     Command to remove a stop from a route's sequence.
+/// </summary>
+/// <param name="RouteId">The route to remove the stop from.</param>
+/// <param name="StopId">The identity of the stop to remove.</param>
+public record RemoveStopCommand(Guid RouteId, Guid StopId);
