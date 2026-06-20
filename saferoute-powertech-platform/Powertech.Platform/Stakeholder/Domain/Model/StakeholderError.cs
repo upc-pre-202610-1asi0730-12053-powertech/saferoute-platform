0efcc8c5fb80ac552e1ecf7e9ab30ff2dc1212c2
Dela050406 <mathias.dela05@gmail.com>
2026-06-20 18:24:36 -0500

    add StakeHolderError

---
+++ saferoute-powertech-platform/Powertech.Platform/Stakeholder/Domain/Model/StakeholderError.cs
@@ -0,0 +1,12 @@
+namespace Powertech.Platform.Stakeholder.Domain.Model;
+
+public enum StakeholderError
+{
+    InvalidStakeholderData,
+    ParentNotFound,
+    DriverNotFound,
+    StudentGroupNotFound,
+    InvalidStudentGroupState,
+    DatabaseError,
+    InternalServerError
+}
