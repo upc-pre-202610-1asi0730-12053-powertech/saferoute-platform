ac4538151fa3dc6c739490d687886bba2ffd657d
Nickolas Ramirez <developernick02@gmail.com>
2026-06-20 15:02:52 -0500

    feat: added AddStopResource for Fleet bounded context

---
+++ saferoute-powertech-platform/Powertech.Platform/Fleet/Interfaces/Rest/Resources/AddStopResource.cs
@@ -0,0 +1,7 @@
+﻿namespace Powertech.Platform.Fleet.Interfaces.Rest.Resources;
+
+/// <summary>Input resource to append a stop to a route.</summary>
+/// <param name="Name">The stop name.</param>
+/// <param name="Latitude">The stop latitude.</param>
+/// <param name="Longitude">The stop longitude.</param>
+public record AddStopResource(string Name, double Latitude, double Longitude);
