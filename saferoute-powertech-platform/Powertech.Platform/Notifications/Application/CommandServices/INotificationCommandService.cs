73a225c7a0753562bd3a265934d8e7a14c927c4f
Frase <ytcarniy@gmail.com>
2026-06-20 18:38:08 -0500

    feat(notification): add create notification command service contract.

---
+++ saferoute-powertech-platform/Powertech.Platform/Notifications/Application/CommandServices/INotificationCommandService.cs
@@ -0,0 +1,10 @@
+using Powertech.Platform.Notifications.Domain.Model.Aggregates;
+using Powertech.Platform.Notifications.Domain.Model.Commands;
+using Powertech.Platform.Shared.Application.Model;
+
+namespace Powertech.Platform.Notifications.Application.CommandServices;
+
+public interface INotificationCommandService
+{
+    Task<Result<Notification>> Handle(CreateNotificationCommand command, CancellationToken cancellationToken = default);
+}
