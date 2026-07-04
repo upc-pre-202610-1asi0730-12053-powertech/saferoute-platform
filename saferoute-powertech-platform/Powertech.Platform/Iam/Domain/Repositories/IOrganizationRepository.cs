5adce36194d16021421519a7adf5ea4e8bd70ba9
Nickolas Ramirez <developernick02@gmail.com>
2026-07-04 03:18:57 -0500

    feat(iam): add IOrganizationRepository

---
+++ saferoute-powertech-platform/Powertech.Platform/Iam/Domain/Repositories/IOrganizationRepository.cs
@@ -0,0 +1,10 @@
+﻿using Powertech.Platform.Iam.Domain.Model.Aggregates;
+using Powertech.Platform.Shared.Domain.Model.ValueObjects;
+using Powertech.Platform.Shared.Domain.Repositories;
+
+namespace Powertech.Platform.Iam.Domain.Repositories;
+
+public interface IOrganizationRepository : IBaseRepository<Organization>
+{
+    Task<Organization?> FindByOrganizationIdAsync(OrganizationId organizationId, CancellationToken cancellationToken);
+}
