3b21dd910cd2f9ff66ee2ed1bcd99896a79b03f9
Frase <ytcarniy@gmail.com>
2026-07-04 05:38:39 -0500

    feat(Iam): add IHashingService interface.

---
+++ saferoute-powertech-platform/Powertech.Platform/Iam/Application/Internal/OutboundServices/IHashingService.cs
@@ -0,0 +1,8 @@
+namespace Powertech.Platform.Iam.Application.Internal.OutboundServices;
+
+public interface IHashingService
+{
+    string HashPassword(string password);
+
+    bool VerifyPassword(string password, string passwordHash);
+}
