eb3dbab05cb13397f55a07aba13361da399dc217
Fabio Vallejo <u20211d989@upc.edu.pe>
2026-07-05 00:50:44 -0500

    feat: added JWT token service for Iam infrastructure

---
+++ saferoute-powertech-platform/Powertech.Platform/Iam/Infrastructure/Tokens/Jwt/Configuration/TokenSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Powertech.Platform.Iam.Infrastructure.Tokens.Jwt.Configuration;
+
+public class TokenSettings
+{
+    public required string Secret { get; set; }
+
+    public int ExpirationDays { get; set; } = 7;
+}
---
+++ saferoute-powertech-platform/Powertech.Platform/Iam/Infrastructure/Tokens/Jwt/Services/TokenService.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using Powertech.Platform.Iam.Application.Internal.OutboundServices;
+using Powertech.Platform.Iam.Domain.Model.Aggregates;
+using Powertech.Platform.Iam.Infrastructure.Tokens.Jwt.Configuration;
+
+namespace Powertech.Platform.Iam.Infrastructure.Tokens.Jwt.Services;
+
+public class TokenService(IOptions<TokenSettings> tokenSettings) : ITokenService
+{
+    private readonly TokenSettings _tokenSettings = tokenSettings.Value;
+
+    public string GenerateToken(User user)
+    {
+        var key = Encoding.UTF8.GetBytes(_tokenSettings.Secret);
+
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.NameIdentifier, user.Id.Identifier.ToString()),
+            new(ClaimTypes.Email, user.Email.Value),
+            new(ClaimTypes.Name, user.GetFullName()),
+            new(ClaimTypes.Role, user.Role.Value)
+        };
+        if (user.OrganizationId is not null)
+            claims.Add(new Claim("organizationId", user.OrganizationId.Identifier.ToString()));
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddDays(_tokenSettings.ExpirationDays),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var tokenHandler = new JsonWebTokenHandler();
+        return tokenHandler.CreateToken(tokenDescriptor);
+    }
+}
